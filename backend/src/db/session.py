3c394f73442707a1756b44542ece6a80012787c5
Vedant005 <vedantkanekar909@gmail.com>
2026-02-02 22:28:51 +0530

    feat: seesion maker for background task

---
+++ backend/src/db/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from src.core.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=False)
+
+# This is what we import in the background task
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
