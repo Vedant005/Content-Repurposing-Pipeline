eda9e57a15f7a2dd883dee2d5e0854eab03f9ce1
Vedant005 <vedantkanekar909@gmail.com>
2026-05-21 21:44:41 +0530

    chore : update allowed origins

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     GROQ_API_KEY: str
     SUPADATA_API_KEY: str
-    ALLOWED_ORIGINS: Union[List[str], str] = ["http://localhost:3000", "https://content-repurposing-pipeline.vercel.app"]
+    ALLOWED_ORIGINS: Union[List[str], str] = ["http://localhost:3000", "https://content-times.vercel.app"]
     ENV: str = "prod"
 
     class Config:
